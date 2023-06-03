55b487d7f689b8366cc85eaa4bc0423228938765
Lonnie Mitchell <81715307+LJMitchellCodes@users.noreply.github.com>
2023-06-03 18:13:09 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "././Pages/Home/Home";
+import About from "././Pages/About/About";
+
+
+
+
+function App() {
+  return (
+    <div>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
